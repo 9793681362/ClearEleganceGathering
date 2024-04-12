b6b378903285f078543326dc59a07ec5be0c921a
9793681362 <135099268+9793681362@users.noreply.github.com>
2024-04-12 17:42:53 +0800

    提交代码

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+let v1 = document.querySelector('.v1')
+let v2 = document.querySelector('.v2')
+let nav_down = document.querySelector('.nav-down')
+let open = document.querySelector('.open')
+
+
+
+v1.addEventListener('click', function () {
+    nav_down.style.display = "flex";
+    console.log(v1);
+})
+
+v2.addEventListener('click', function () {
+    window.location.assign('http://www.baidu.com')
+})
+
+open.addEventListener('click', function () {
+    window.location.href = 'http://rick1001b.cn/open/'
+})
+
+
